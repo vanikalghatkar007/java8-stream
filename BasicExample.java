cdea7fe16e0483e3e01eb43d3b088af5b95148f2
vanikalghatkar007 <68254967+vanikalghatkar007@users.noreply.github.com>
2022-04-21 09:59:44 +0530

    Arrays Example

---
+++ BasicExample.java
@@ -0,0 +1,19 @@
+package com.Arrays;
+
+import java.util.Scanner;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a[], n;
+		System.out.print("Enter number of array elements");
+		n=sc.nextInt();
+		for (int i=0;i<n;i++) {
+			System.out.print(a[i]);
+		}
+
+	}
+
+}
